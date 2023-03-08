b6302ff95df4013282a052ebb3543eb2dbc89f0a
albrja <albrja@uw.edu>
2023-03-08 12:19:15 -0800

    Moving vectorized choice to noise functions.py to avoid circular dependencies

--- src/pseudopeople/utilities.py
+++ src/pseudopeople/utilities.py
@@ -10,37 +10,3 @@ from pseudopeople.entities import Form
 
 def get_randomness_stream(form: Form, seed: int) -> RandomnessStream:
     return RandomnessStream(form.value, lambda: pd.Timestamp("2020-04-01"), seed)
-
-
-def vectorized_choice(
-    options: Union[pd.Series, np.array],
-    n_to_choose: int,
-    randomness_stream: RandomnessStream = None,
-    weights: Union[pd.Series, np.array] = None,
-    additional_key: Any = None,
-    random_seed: int = None,
-):
-    if not randomness_stream and (additional_key == None and random_seed == None):
-        raise RuntimeError(
-            "An additional_key and a random_seed are required in 'vectorized_choice'"
-            + "if no RandomnessStream is passed in"
-        )
-    if weights is None:
-        n = len(options)
-        weights = np.ones(n) / n
-    # for each of n_to_choose, sample uniformly between 0 and 1
-    index = pd.Index(np.arange(n_to_choose))
-    if randomness_stream is None:
-        # Generate an additional_key on-the-fly and use that in randomness.random
-        additional_key = f"{additional_key}_{random_seed}"
-        probs = random(str(additional_key), index)
-    else:
-        probs = randomness_stream.get_draw(index, additional_key=additional_key)
-
-    # build cdf based on weights
-    pmf = weights / weights.sum()
-    cdf = np.cumsum(pmf)
-
-    # for each p_i in probs, count how many elements of cdf for which p_i >= cdf_i
-    chosen_indices = np.searchsorted(cdf, probs, side="right")
-    return np.take(options, chosen_indices)
