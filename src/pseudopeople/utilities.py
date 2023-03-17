27f0f963615bbd6016a29391550df80c723b39f2
Matthew Kappel <mkappel@uw.edu>
2023-03-17 16:57:35 -0700

    full implementation and test

--- src/pseudopeople/utilities.py
+++ src/pseudopeople/utilities.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Union
 
 import pandas as pd
 from vivarium.framework.configuration import ConfigTree, ConfigurationError
@@ -16,9 +17,23 @@ def get_default_configuration() -> ConfigTree:
 
     default_config_layers = [
         "base",
+        "user",
     ]
     noising_configuration = ConfigTree(layers=default_config_layers)
     BASE_DIR = Path(pseudopeople.__file__).resolve().parent
     yaml_path = BASE_DIR / "default_configuration.yaml"
     noising_configuration.update(yaml_path, layer="base")
     return noising_configuration
+
+
+def update_configuration_with_yaml(
+    configuration: ConfigTree, yaml_path: Union[Path, str]
+) -> ConfigTree:
+    """
+    Updates a configuration ConfigTree with overrides from a provided YAML file.
+
+    :param configuration: A ConfigTree configuration to override with a given YAML file
+    :param yaml_path: A path to the YAML file defining overrides for configuration
+    :return: a ConfigTree object updated with the configuration from the YAML
+    """
+    return configuration.update(yaml_path, layer="user")
--- tests/unit/test_configuration.py
+++ tests/unit/test_configuration.py
@@ -1,8 +1,26 @@
+from pathlib import Path
+
 import pytest
 from vivarium.config_tree import ConfigTree
 
 from pseudopeople.entities import Form
-from pseudopeople.utilities import get_default_configuration
+from pseudopeople.utilities import (
+    get_default_configuration,
+    update_configuration_with_yaml,
+)
+
+
+@pytest.fixture
+def user_configuration_yaml(tmp_path):
+    text = """decennial_census:
+    omission: 0.05
+    first_name:
+        nickname:
+            row_noise_level: 0.05"""
+    user_config_path = Path(f"{tmp_path}/test_configuration.yaml")
+    with open(user_config_path, "w") as file:
+        file.write(text)
+    return user_config_path
 
 
 def test_default_configuration():
@@ -10,12 +28,17 @@ def test_default_configuration():
     assert config
     assert isinstance(config, ConfigTree)
     # TODO: From Rajan: We should test that this configuration actually matches
-    # what we'd expect it to be. We can do this either by comparing it to the
-    # values in the yaml file, or by just confirming that the correct call to
-    # config_tree.update() was made in the function. The latter seems preferable
-    # to me as a unit test.
+    #  what we'd expect it to be. We can do this either by comparing it to the
+    #  values in the yaml file, or by just confirming that the correct call to
+    #  config_tree.update() was made in the function. The latter seems preferable
+    #  to me as a unit test.
 
 
-@pytest.mark.skip(reason="TODO")
-def test_user_configuration_file():
-    pass
+def test_user_configuration_file(user_configuration_yaml):
+    """Test that a user config yaml will override the default configuration."""
+    config = get_default_configuration()
+    config = update_configuration_with_yaml(config, user_configuration_yaml)
+    assert (
+        config["decennial_census"]["omission"]
+        == config["decennial_census"]["first_name"]["nickname"]["row_noise_level"]
+    )
