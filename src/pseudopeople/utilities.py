a0303f025293c56b30bd47a759f868ce9bfc4065
albrja <albrja@uw.edu>
2023-03-23 16:07:38 -0700

    Adding vectorized_choice to utilities

--- src/pseudopeople/utilities.py
+++ src/pseudopeople/utilities.py
@@ -1,9 +1,10 @@
 from pathlib import Path
-from typing import Union
+from typing import Any, Union
 
+import numpy as np
 import pandas as pd
 from vivarium.framework.configuration import ConfigTree
-from vivarium.framework.randomness import RandomnessStream
+from vivarium.framework.randomness import RandomnessStream, random
 
 from pseudopeople.entities import Form
 
@@ -32,3 +33,37 @@ def get_configuration(user_yaml_path: Union[Path, str] = None) -> ConfigTree:
     if user_yaml_path:
         noising_configuration.update(user_yaml_path, layer="user")
     return noising_configuration
+
+
+def vectorized_choice(
+    options: Union[list, pd.Series],
+    n_to_choose: int,
+    randomness_stream: RandomnessStream = None,
+    weights: Union[list, pd.Series] = None,
+    additional_key: Any = None,
+    random_seed: int = None,
+):
+    if not randomness_stream and (additional_key == None and random_seed == None):
+        raise RuntimeError(
+            "An additional_key and a random_seed are required in 'vectorized_choice'"
+            + "if no RandomnessStream is passed in"
+        )
+    if weights is None:
+        n = len(options)
+        weights = np.ones(n) / n
+    # for each of n_to_choose, sample uniformly between 0 and 1
+    index = pd.Index(np.arange(n_to_choose))
+    if randomness_stream is None:
+        # Generate an additional_key on-the-fly and use that in randomness.random
+        additional_key = f"{additional_key}_{random_seed}"
+        probs = random(str(additional_key), index)
+    else:
+        probs = randomness_stream.get_draw(index, additional_key=additional_key)
+
+    # build cdf based on weights
+    pmf = weights / weights.sum()
+    cdf = np.cumsum(pmf)
+
+    # for each p_i in probs, count how many elements of cdf for which p_i >= cdf_i
+    chosen_indices = np.searchsorted(cdf, probs, side="right")
+    return np.take(options, chosen_indices)
