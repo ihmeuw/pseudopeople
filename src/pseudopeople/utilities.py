f1f05582e395351a4485c90fd31bd9b7a4d5a00f
Matthew Kappel <mkappel@uw.edu>
2023-04-03 15:52:49 -0700

    full implementation, int test pending mic-3884

--- src/pseudopeople/utilities.py
+++ src/pseudopeople/utilities.py
@@ -13,11 +13,11 @@ def get_randomness_stream(form: Form, seed: int) -> RandomnessStream:
     return RandomnessStream(form.value, lambda: pd.Timestamp("2020-04-01"), seed)
 
 
-def get_configuration(user_yaml_path: Union[Path, str] = None) -> ConfigTree:
+def get_configuration(user_configuration: Union[Path, str, dict] = None) -> ConfigTree:
     """
     Gets a noising configuration ConfigTree, optionally overridden by a user-provided YAML.
 
-    :param user_yaml_path: A path to the YAML file defining user overrides for the defaults
+    :param user_configuration: A path to the YAML file defining user overrides for the defaults
     :return: a ConfigTree object of the noising configuration
     """
     import pseudopeople
@@ -30,8 +30,8 @@ def get_configuration(user_yaml_path: Union[Path, str] = None) -> ConfigTree:
         data=Path(pseudopeople.__file__).resolve().parent / "default_configuration.yaml",
         layers=default_config_layers,
     )
-    if user_yaml_path:
-        noising_configuration.update(user_yaml_path, layer="user")
+    if user_configuration:
+        noising_configuration.update(user_configuration, layer="user")
     return noising_configuration
 
 
