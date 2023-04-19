a57564c7f42b149e1a2dde87a007a0ed040e5701
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2023-04-19 13:20:42 -0700

    update all hadcoded references to dataset names and acs interface function name

--- tests/unit/test_row_noise.py
+++ tests/unit/test_row_noise.py
@@ -4,7 +4,6 @@ import pytest
 from vivarium.framework.randomness import RandomnessStream
 
 from pseudopeople.configuration import Keys, get_configuration
-from pseudopeople.constants import metadata
 from pseudopeople.noise_entities import NOISE_TYPES
 from pseudopeople.schema_entities import DATASETS
 
@@ -37,7 +36,7 @@ def test_omission(dummy_data):
         NOISE_TYPES.omission.name
     ]
     dataset_name_1 = "dummy_dataset_name"
-    dataset_name_2 = metadata.DatasetNames.ACS
+    dataset_name_2 = DATASETS.acs.name
     noised_data1 = NOISE_TYPES.omission(dataset_name_1, dummy_data, config, RANDOMNESS)
     noised_data2 = NOISE_TYPES.omission(dataset_name_2, dummy_data, config, RANDOMNESS)
 
