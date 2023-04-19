9cdada6c57c80f8a1e8daf015db1496a630bd5e7
albrja <albrja@uw.edu>
2023-04-19 10:52:58 -0700

    Added module doc string

--- src/pseudopeople/configuration/interface.py
+++ src/pseudopeople/configuration/interface.py
@@ -1,4 +1,18 @@
 """
+===========================
+The Configuration Interface
+===========================
+
+An interface for user's to interact with the configuration (config) in pseudopeople.
+
+In ``pseudopeople`` a configuration is used to provide noise level that get passed to the noise functions to apply
+various types of noise to given datasets.  Users can provide their own configuration that will override the deault
+values.
+.. code-block:: python
+    >>> import pseudopeople as pp
+    >>> pp.get_config("decennial_census")
+    >>> user_config = {"decennial_census": {"row_noise": {"probability": 0.1},},}
+    >>> pp.get_config("decennial_census", user_config)
 
 """
 from pathlib import Path
@@ -11,25 +25,27 @@ from pseudopeople.constants.metadata import FORM_NAMES
 
 
 def get_config(
-    form_name: str = None, user_configuration: Union[Path, str, Dict] = None
+    dataset_name: str = None, user_configuration: Union[Path, str, Dict] = None
 ) -> Dict:
     """
     Function that displays the configuration for the user
-    :param form_name: Name of form to lookup in configuration.  Providing this argument returns the configuration for
+    :param dataset_name: Name of dataset to lookup in configuration.  Providing this argument returns the configuration for
       this specific form and no other forms in the configuration.
     :param user_configuration: Dictionary of configuration values the user wishes to manually override.
     """
 
     config = get_configuration(user_configuration)
-    if form_name:
-        if form_name in FORM_NAMES:
-            config = config[form_name]
-        elif form_name not in FORM_NAMES:
-            raise ValueError(f"{form_name} provided but is not a valid option for form type.")
+    if dataset_name:
+        if dataset_name in FORM_NAMES:
+            config = config[dataset_name]
+        elif dataset_name not in FORM_NAMES:
+            raise ValueError(
+                f"{dataset_name} provided but is not a valid option for dataset type."
+            )
     if user_configuration:
-        if form_name not in user_configuration.keys():
+        if dataset_name not in user_configuration.keys():
             logger.warning(
-                f"{form_name} provided but is not in the user provided configuration."
+                f"{dataset_name} provided but is not in the user provided configuration."
             )
 
     return config.to_dict()
