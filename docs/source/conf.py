936a15f76952d9f16b2480f1e4d487a125d4a067
Alix Pletcher <51676259+pletale@users.noreply.github.com>
2023-04-24 11:36:55 -0700

    Update docs/source/conf.py
    Co-authored-by: Zeb Burke-Conte <zmbc@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -107,6 +107,9 @@ add_module_names = False
 html_theme_path = ["_theme"]
 html_theme = "sphinx_rtd_theme"
 html_logo = "_static/pseudopeople_logo_white.png"
+html_theme_options = {
+    'logo_only': True,
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
