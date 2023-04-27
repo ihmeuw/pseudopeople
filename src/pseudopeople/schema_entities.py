29dab49f0eb81b6a7d3dc66c51432f3b3463dbe6
Matthew Kappel <mkappel@uw.edu>
2023-04-27 14:25:54 -0700

    Implement targeted omission do_not_respond for census and surveys (#158)
    - *Category*: feature
    - *JIRA issue*: [MIC-3934](https://jira.ihme.washington.edu/browse/MIC-3934)
    
    Changes
    - Adds do_not_respond noising for census, ACS, and CPS
    - Updates omission tests for the do_not_respond change
    - Adds tests for do_not_respond proportionality and ports the oversampling testing previously done in the omission/omit_rows testing
    - Adds test for incorrect dataset application of do_not_respond
    - Adds test for omit_rows/do_not_respond mutual exclusivity 
    - Adds test for correctness of census race/ethnicity, age, sex noise level adjustments
    
    Testing
    All tests work.

--- src/pseudopeople/schema_entities.py
+++ src/pseudopeople/schema_entities.py
@@ -359,11 +359,7 @@ class Dataset:
     name: str
     columns: Tuple[Column, ...]  # This defines the output column order
     date_column: str
-
-    row_noise_types: Tuple[RowNoiseType, ...] = (
-        NOISE_TYPES.omission,
-        # NOISE_TYPES.duplication,
-    )
+    row_noise_types: Tuple[RowNoiseType, ...]
 
 
 class __Datasets(NamedTuple):
@@ -389,6 +385,10 @@ class __Datasets(NamedTuple):
             COLUMNS.race_ethnicity,
         ),
         date_column="year",
+        row_noise_types=(
+            NOISE_TYPES.do_not_respond,
+            # NOISE_TYPES.duplication,
+        ),
     )
     acs: Dataset = Dataset(
         DatasetNames.ACS,
@@ -411,6 +411,10 @@ class __Datasets(NamedTuple):
             COLUMNS.race_ethnicity,
         ),
         date_column="survey_date",
+        row_noise_types=(
+            NOISE_TYPES.do_not_respond,
+            # NOISE_TYPES.duplication,
+        ),
     )
     cps: Dataset = Dataset(
         DatasetNames.CPS,
@@ -433,6 +437,10 @@ class __Datasets(NamedTuple):
             COLUMNS.race_ethnicity,
         ),
         date_column="survey_date",
+        row_noise_types=(
+            NOISE_TYPES.do_not_respond,
+            # NOISE_TYPES.duplication,
+        ),
     )
     wic: Dataset = Dataset(
         DatasetNames.WIC,
@@ -453,6 +461,10 @@ class __Datasets(NamedTuple):
             COLUMNS.race_ethnicity,
         ),
         date_column="year",
+        row_noise_types=(
+            NOISE_TYPES.omission,
+            # NOISE_TYPES.duplication,
+        ),
     )
     ssa: Dataset = Dataset(
         DatasetNames.SSA,
@@ -467,6 +479,10 @@ class __Datasets(NamedTuple):
             COLUMNS.ssa_event_date,
         ),
         date_column="event_date",
+        row_noise_types=(
+            NOISE_TYPES.omission,
+            # NOISE_TYPES.duplication,
+        ),
     )
     tax_w2_1099: Dataset = Dataset(
         DatasetNames.TAXES_W2_1099,
@@ -497,6 +513,10 @@ class __Datasets(NamedTuple):
             COLUMNS.tax_form,
         ),
         date_column="tax_year",
+        row_noise_types=(
+            NOISE_TYPES.omission,
+            # NOISE_TYPES.duplication,
+        ),
     )
     # tax_1040: Dataset = Dataset(
     #     Datasets.TAXES_1040,
