c760990066568abf922753a5c38889b761c08a3c
albrja <37345113+albrja@users.noreply.github.com>
2023-07-20 14:51:45 -0700

    Mic 4210/1040 implementation (#228)
    Title: Mic 4210/1040 implementation
    
    Description
    Implementation for 1040
    - *Category*: Feature
    - *JIRA issue*: [MIC-4210](https://jira.ihme.washington.edu/browse/MIC-4210)
    
    -adds generate_tax_1040 function to interface.py
    -updates metadata constants for additional 1040 columns for noising
    -updates W2 date of birth config to be 0.0
    
    Testing
    All tests pass but test suites will be updated in the next ticket to incorporate the 1040.

--- src/pseudopeople/constants/metadata.py
+++ src/pseudopeople/constants/metadata.py
@@ -88,5 +88,10 @@ COPY_HOUSEHOLD_MEMBER_COLS = {
     "age": "copy_age",
     "date_of_birth": "copy_date_of_birth",
     "ssn": "copy_ssn",
+    "spouse_ssn": "spouse_copy_ssn",
+    "dependent_1_ssn": "dependent_1_copy_ssn",
+    "dependent_2_ssn": "dependent_2_copy_ssn",
+    "dependent_3_ssn": "dependent_3_copy_ssn",
+    "dependent_4_ssn": "dependent_4_copy_ssn",
     # TODO: add itins
 }
