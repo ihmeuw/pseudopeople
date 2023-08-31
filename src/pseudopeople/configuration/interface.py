f2c2889a05ac81359c554c2e9a50ab5798610cde
albrja <albrja@uw.edu>
2023-08-31 06:29:32 -0700

    Remove dataset name from get_config and update docstring

--- src/pseudopeople/configuration/interface.py
+++ src/pseudopeople/configuration/interface.py
@@ -10,22 +10,15 @@ from pseudopeople.exceptions import ConfigurationError
 from pseudopeople.schema_entities import DATASETS
 
 
-def get_config(dataset_name: str = None, overrides: Union[Path, str, Dict] = None) -> Dict:
+def get_config(overrides: Union[Path, str, Dict] = None) -> Dict:
     """
-    Function that returns the pseudopeople configuration,
-    including all default values.
-    If :code:`dataset_name` is None (the default), the returned dictionary has exactly
-    the structure described on the :ref:`Configuration page <configuration_main>`.
-    If a dataset name is supplied, only returns the part of the configuration for that
-    dataset, i.e. :code:`get_config(dataset_name)` is equivalent to
-    :code:`get_config()[dataset_name]`.
-
+    Function that returns the pseudopeople configuration for all default values.
     To get the default probability of nonresponse in the Decennial Census dataset:
 
     .. code-block:: pycon
 
         >>> import pseudopeople as psp
-        >>> psp.get_config('decennial_census')['row_noise']['do_not_respond']
+        >>> psp.get_config()[]'decennial_census']['row_noise']['do_not_respond']
         {'row_probability': 0.0145}
 
     To view that same part of the configuration after applying a user override:
@@ -33,21 +26,9 @@ def get_config(dataset_name: str = None, overrides: Union[Path, str, Dict] = Non
     .. code-block:: pycon
 
         >>> overrides = {'decennial_census': {'row_noise': {'do_not_respond': {'row_probability': 0.1}}}}
-        >>> psp.get_config('decennial_census', overrides)['row_noise']['do_not_respond']
+        >>> psp.get_config(overrides)["decenial_census']['row_noise']['do_not_respond']
         {'row_probability': 0.1}
 
-    :param dataset_name: An optional name of dataset to return the configuration
-        for (defaults to all dataset configurations). Providing this argument returns
-        the configuration for this specific dataset and no other datasets that exist
-        in the configuration. Possible dataset names include:
-
-            - "american_community_survey"
-            - "decennial_census"
-            - "current_population_survey"
-            - "social_security"
-            - "taxes_1040"
-            - "taxes_w2_and_1099"
-            - "women_infants_and_children"
     :param overrides: An optional override to the default configuration. Can be
         a path to a configuration YAML file or a dictionary. Passing a sentinel value
         of psp.NO_NOISE will override default values and return a configuration
@@ -59,17 +40,6 @@ def get_config(dataset_name: str = None, overrides: Union[Path, str, Dict] = Non
     if isinstance(overrides, (Path, str)) and overrides != NO_NOISE:
         with open(overrides, "r") as f:
             overrides = yaml.full_load(f)
-    if isinstance(overrides, dict) and dataset_name not in overrides.keys():
-        logger.warning(
-            f"'{dataset_name}' provided but is not in the user provided configuration."
-        )
     config = get_configuration(overrides).to_dict()
-    if dataset_name:
-        if dataset_name in [dataset.name for dataset in DATASETS]:
-            config = {dataset_name: config[dataset_name]}
-        else:
-            raise ConfigurationError(
-                f"'{dataset_name}' provided but is not a valid option for dataset type."
-            )
 
     return config
