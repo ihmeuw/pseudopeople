bbabfdfc61a7552cf7f300511fe909e4ed185ec2
Nathaniel Blair-Stahn <ndbs@uw.edu>
2023-09-08 16:52:30 -0700

    clean up ACS docstring wrapping

--- src/pseudopeople/interface.py
+++ src/pseudopeople/interface.py
@@ -247,26 +247,44 @@ def generate_american_community_survey(
     citizenship, education, income, language proficiency, migration, employment,
     disability, and housing characteristics.
 
-    :param source: The root directory containing pseudopeople simulated population data. Defaults
-        to using the included sample population when source is `None`.
-    :param seed: An integer seed for randomness. Defaults to 0.
-    :param config: An optional override to the default configuration. Can be a path
-        to a configuration YAML file, a configuration dictionary,
-        or the sentinel value `pseudopeople.NO_NOISE`, which will generate a
-        dataset without any configurable noise.
-    :param year: The year for which to generate simulated American Community Surveys of the simulated population (format YYYY, e.g., 2036);
-        the simulated dataset will contain records for surveys conducted on any date in the specified year.
-        Will
-        raise a `ValueError` if there is no data for this year. Default is 2020. If `None` is passed instead, data for all available years are
-        included in the returned dataset.
-    :param state: The state string to include in the dataset. Either full name or
-        abbreviation (e.g., "Ohio" or "OH"). Will raise a ValueError if there is no
-        data for this state. If None is provided, data from all locations are
+    :param source:
+        The root directory containing pseudopeople simulated population data.
+        Defaults to using the included sample population when source is `None`.
+
+    :param seed:
+        An integer seed for randomness. Defaults to 0.
+
+    :param config:
+        An optional override to the default configuration. Can be a path to a
+        configuration YAML file, a configuration dictionary, or the sentinel
+        value `pseudopeople.NO_NOISE`, which will generate a dataset without any
+        configurable noise.
+
+    :param year:
+        The year for which to generate simulated American Community Surveys of
+        the simulated population (format YYYY, e.g., 2036); the simulated
+        dataset will contain records for surveys conducted on any date in the
+        specified year. Will raise a `ValueError` if there is no data for this
+        year. Default is 2020. If `None` is passed instead, data for all
+        available years are included in the returned dataset.
+
+    :param state:
+        The state string to include in the dataset. Either full name or
+        abbreviation (e.g., "Ohio" or "OH"). Will raise a ValueError if there is
+        no data for this state. If None is provided, data from all locations are
         included in the dataset.
-    :param verbose: Log with verbosity if `True`. Default is `False`.
-    :return: A `pandas.DataFrame` of simulated ACS data.
-    :raises ConfigurationError: An incorrect config is provided.
-    :raises DataSourceError: An incorrect pseudopeople simulated population data source is provided.
+
+    :param verbose:
+        Log with verbosity if `True`. Default is `False`.
+
+    :return:
+        A `pandas.DataFrame` of simulated ACS data.
+
+    :raises ConfigurationError:
+        An incorrect config is provided.
+
+    :raises DataSourceError:
+        An incorrect pseudopeople simulated population data source is provided.
     """
     user_filters = []
     if year is not None:
