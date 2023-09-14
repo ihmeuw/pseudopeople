22607a915205ea0eb178e39ff4e5606139d41e3d
Nathaniel Blair-Stahn <ndbs@uw.edu>
2023-09-13 23:43:48 -0700

    edit description again

--- src/pseudopeople/configuration/interface.py
+++ src/pseudopeople/configuration/interface.py
@@ -6,7 +6,7 @@ from pseudopeople.configuration.generator import get_configuration
 
 def get_config(overrides: Union[Path, str, Dict] = None) -> Dict:
     """
-    Function that returns the pseudopeople configuration including all
+    Function that returns the pseudopeople configuration containing all
     default values. To get the default probability of nonresponse in the
     Decennial Census dataset:
 
@@ -30,15 +30,15 @@ def get_config(overrides: Union[Path, str, Dict] = None) -> Dict:
         An optional override to the default configuration. Can be a
         Python dictionary, a path to a YAML file, or the special
         sentinel value `pseudopeople.NO_NOISE`, which will return a
-        configuration where all configurable noise levels are set to 0.
-        When passing a configuration dictionary or YAML configuration
-        file, it is not necessary to provide a complete configuration;
-        any configuration parameters not specified in `overrides` will
-        be filled in with the default values.
+        configuration specifying that all configurable noise is turned
+        off. When passing a dictionary or YAML file, it is not necessary
+        to provide a complete configuration; any configuration
+        parameters not specified in `overrides` will be filled in with
+        the default values.
 
     :return:
 
-        Dictionary representing the configuration.
+        A complete configuration dictionary.
 
     :raises ConfigurationError:
 
