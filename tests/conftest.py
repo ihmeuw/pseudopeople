9f162f9f491defc07751ce224ca096422af99110
albrja <37345113+albrja@users.noreply.github.com>
2023-10-16 14:21:41 -0700

    Mic 4563/update for slow tests (#319)
    Mic-4563/update for slow tests
    
    Adds --limit flag to limit number of parametrize iterations run on a test function
    - *Category*: Test
    - *JIRA issue*: [MIC-4563](https://jira.ihme.washington.edu/browse/MIC-4563)
    
    -adds --limit flag in confest test along with tests to use this flag on. Currently only test_noise_order
    -adds a new test for write_wrong_digits which is an accuracy test for magnitude and marks old more robust test as slow
    
    Testing
    All tests pass. Limit flag works as intended.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,13 @@ from loguru import logger
 
 def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true", default=False, help="run slow tests")
+    parser.addoption(
+        "--limit",
+        action="store",
+        default=-1,
+        type=int,
+        help="Maximum number of parameterized tests to run",
+    )
 
 
 def pytest_configure(config):
@@ -24,6 +31,18 @@ def pytest_collection_modifyitems(config, items):
             item.add_marker(pytest.mark.slow)
         if "slow" in item.keywords:
             item.add_marker(skip_slow)
+
+    # Limit the number of permutations of parametrised tests to run.
+    limit = config.getoption("--limit")
+    if limit > 0:
+        tests_by_name = {item.name: item for item in items}
+        # Add the name of parametrized base tests to this list.
+        tests_to_skip_parametrize = ["test_noise_order"]
+
+        for base_name in tests_to_skip_parametrize:
+            to_skip = [t for n, t in tests_by_name.items() if base_name in n][limit:]
+            for t in to_skip:
+                t.add_marker("skip")
 
 
 @pytest.fixture
