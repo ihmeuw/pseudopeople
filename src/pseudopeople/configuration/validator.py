dfd697a18cc294f79e2f6d20f06e2e7afc777c8a
albrja <37345113+albrja@users.noreply.github.com>
2023-11-30 16:33:14 -0800

    Mic 4671/user warnings (#353)
    Mic 4671/user warnings
    
    Updates user warnings when invalid noise levels are provided by users.
    - *Category*: Feature
    - *JIRA issue*: [MIC-4671](https://jira.ihme.washington.edu/browse/MIC-4671)
    
    -Adds metadata proportion file to validate noise levels to
    -Throws warning to users when they provide a value larger than the calculated max level noise level from metadata proportions file
    -Updates tests to reflect feature updates
    
    Testing
    All tests pass

--- src/pseudopeople/configuration/validator.py
+++ src/pseudopeople/configuration/validator.py
@@ -1,14 +1,16 @@
-from typing import Callable, Dict, List, Union
+from typing import Callable, Dict, List, Tuple, Union
 
 import numpy as np
+import pandas as pd
 from loguru import logger
 from vivarium.config_tree import ConfigTree, ConfigurationKeyError
 
 from pseudopeople.configuration import Keys
-from pseudopeople.constants import metadata
+from pseudopeople.constants import metadata, paths
 from pseudopeople.exceptions import ConfigurationError
 from pseudopeople.noise_entities import NOISE_TYPES
 from pseudopeople.noise_scaling import get_options_for_column
+from pseudopeople.schema_entities import Dataset
 
 
 def validate_overrides(overrides: Dict, default_config: ConfigTree) -> None:
@@ -88,9 +90,6 @@ def validate_overrides(overrides: Dict, default_config: ConfigTree) -> None:
                     default_column_config, noise_type, "noise type", dataset, column
                 )
                 parameter_config_validator_map = {
-                    NOISE_TYPES.use_nickname.name: {
-                        Keys.CELL_PROBABILITY: _validate_nickname_probability
-                    },
                     NOISE_TYPES.choose_wrong_option.name: {
                         Keys.CELL_PROBABILITY: lambda *args, **kwargs: _validate_choose_wrong_option_probability(
                             *args, **kwargs, column=column
@@ -241,19 +240,6 @@ def _validate_probability(
         )
 
 
-def _validate_nickname_probability(
-    noise_type_config: Union[int, float], parameter: str, base_error_message: str
-) -> None:
-    _validate_probability(noise_type_config, parameter, base_error_message)
-    if noise_type_config > metadata.NICKNAMES_PROPORTION:
-        logger.warning(
-            base_error_message
-            + f"The configured '{parameter}' is {noise_type_config}, but only approximately "
-            f"{metadata.NICKNAMES_PROPORTION:.2%} of names have a nickname. "
-            f"Replacing as many names with nicknames as possible."
-        )
-
-
 def _validate_choose_wrong_option_probability(
     noise_type_config: Union[int, float], parameter: str, base_error_message: str, column: str
 ):
@@ -270,6 +256,84 @@ def _validate_choose_wrong_option_probability(
             f"can only choose the wrong option with a maximum of {maximum_noise_probability:.5f} probability. "
             f"This maximum will be used instead of the configured value."
         )
+
+
+def validate_noise_level_proportions(
+    configuration_tree: ConfigTree, dataset: Dataset, user_filters: List[Tuple]
+) -> None:
+    """
+    Validates that the noise levels provided do not exceed the allowable proportions from the
+    metadata proportions file. If the provided noise levels are higher than the allowable proportions
+    then throw a warning to the user and adjust the noise level to the allowable proportion.
+    """
+    # TODO: update file and filepath
+    metadata_proportions = pd.read_csv(paths.METADATA_PROPORTIONS)
+    dataset_proportions = metadata_proportions.loc[
+        metadata_proportions["dataset"] == dataset.name
+    ]
+    # Set default values for state and year
+    if dataset.name == metadata.DatasetNames.SSA:
+        state = "USA"
+    else:
+        # Note: This is a shortcoming of our current approach to user warnings and will be fixed
+        # with a future release/our next data upload. We do not have a way to get state in a
+        # case where the user is not filtering on state because they either are using the sample
+        # data or state (Rhode Island) data.
+        if len(dataset_proportions["state"].unique()) == 1:
+            state = dataset_proportions["state"].unique()[0]
+        else:
+            state = "USA"
+    year = metadata.YEAR_AGGREGATION_VALUE
+    # Get the state and year from the user filters
+    for i in range(len(user_filters)):
+        if user_filters[i][0] == dataset.state_column_name:
+            state = user_filters[i][2]
+            break
+    for i in range(len(user_filters)):
+        if user_filters[i][0] == dataset.date_column_name:
+            if isinstance(user_filters[i][2], pd.Timestamp):
+                year = user_filters[i][2].year
+            else:
+                year = user_filters[i][2]
+            break
+
+    # Weight SSA since year filter is queried and all proceeding years
+    if dataset.name == metadata.DatasetNames.SSA:
+        dataset_noise_proportions = dataset_proportions.loc[
+            (dataset_proportions["state"] == state) & (dataset_proportions["year"] <= year)
+        ]
+        dataset_noise_proportions = dataset_proportions.groupby(
+            ["column", "noise_type"]
+        ).apply(lambda x: ((x.proportion * x.number_of_rows).sum()) / x.number_of_rows.sum())
+        dataset_noise_proportions = dataset_noise_proportions.rename(
+            "proportion"
+        ).reset_index()
+        dataset_noise_proportions["dataset"] = metadata.DatasetNames.SSA
+    else:
+        dataset_noise_proportions = dataset_proportions.loc[
+            (dataset_proportions["state"] == state) & (dataset_proportions["year"] == year)
+        ]
+
+    # If there is no data for a queried dataset, we want the user's to hit the correct error that there
+    # is no data available so we do not throw an error here.
+    if dataset_noise_proportions.empty:
+        return configuration_tree
+    else:
+        # Go through each row in the queried dataset noise proportions to validate the noise levels
+        for i in range(len(dataset_noise_proportions)):
+            row = dataset_noise_proportions.iloc[i]
+            if row["column"] not in [col.name for col in dataset.columns]:
+                continue
+            max_noise_level = row["proportion"]
+            config_noise_level = configuration_tree[row["dataset"]][Keys.COLUMN_NOISE][
+                row["column"]
+            ][row["noise_type"]][Keys.CELL_PROBABILITY]
+            if config_noise_level > max_noise_level:
+                logger.warning(
+                    f"The configured '{row['noise_type']}' noise level for column '{row['column']}' is {config_noise_level}, "
+                    f"which is higher than the maximum possible value based on the provided data for '{row['dataset']}'. "
+                    "Noising as many rows as possible. "
+                )
 
 
 DEFAULT_PARAMETER_CONFIG_VALIDATOR_MAP = {
--- src/pseudopeople/interface.py
+++ src/pseudopeople/interface.py
@@ -14,7 +14,6 @@ from pseudopeople.constants import paths
 from pseudopeople.constants.metadata import (
     COPY_HOUSEHOLD_MEMBER_COLS,
     DATEFORMATS,
-    DATA_VERSION,
     INT_COLUMNS,
 )
 from pseudopeople.exceptions import DataSourceError
@@ -55,7 +54,7 @@ def _generate_dataset(
         Noised dataset data in a pd.DataFrame
     """
     configure_logging_to_terminal(verbose)
-    configuration_tree = get_configuration(config)
+    configuration_tree = get_configuration(config, dataset, user_filters)
 
     if source is None:
         source = paths.SAMPLE_DATA_ROOT
@@ -110,19 +109,19 @@ def _generate_dataset(
 
 def validate_source_compatibility(source: Path):
     # TODO [MIC-4546]: Clean this up w/ metadata and update test_interface.py tests to be generic
-    metadata = source / "metadata.yaml"
-    if metadata.exists():
-        version = _get_data_version(metadata)
-        if version > parse(DATA_VERSION):
+    changelog = source / "CHANGELOG.rst"
+    if changelog.exists():
+        version = _get_data_changelog_version(changelog)
+        if version > parse("1.4.2"):
             raise DataSourceError(
                 f"The provided simulated population data is incompatible with this version of pseudopeople ({psp_version}).\n"
                 "A newer version of simulated population data has been provided.\n"
                 "Please upgrade the pseudopeople package."
             )
-        if version < parse(DATA_VERSION):
+        if version < parse("1.4.2"):
             raise DataSourceError(
                 f"The provided simulated population data is incompatible with this version of pseudopeople ({psp_version}).\n"
-                "Current data version is {version} but a newer version {DATA_VERSION} is available.\n"
+                "The simulated population data has been corrupted.\n"
                 "Please re-download the simulated population data."
             )
     else:
@@ -133,11 +132,11 @@ def validate_source_compatibility(source: Path):
         )
 
 
-def _get_data_version(metadata_path: Path):
-    with open(metadata_path) as f:
-        metadata = yaml.safe_load(f)
-        version = metadata["data_version"]
-    return parse(version)
+def _get_data_changelog_version(changelog):
+    with open(changelog, "r") as file:
+        first_line = file.readline()
+    version = parse(first_line.split("**")[1].split("-")[0].strip())
+    return version
 
 
 def _coerce_dtypes(
