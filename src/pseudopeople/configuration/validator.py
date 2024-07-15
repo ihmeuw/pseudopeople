b0acd61a90db14666eef7ac5b0309d43fbcec18f
Steve Bachmeier <sbachmei@uw.edu>
2024-07-15 08:13:12 -0700

    change Dict to dict

--- src/pseudopeople/configuration/validator.py
+++ src/pseudopeople/configuration/validator.py
@@ -108,7 +108,7 @@ def validate_overrides(overrides: Dict, default_config: LayeredConfigTree) -> No
 
 
 def _validate_noise_type_config(
-    noise_type_config: Dict,
+    noise_type_config: dict,
     default_noise_type_config: LayeredConfigTree,
     dataset_name: str,
     noise_type: str,
