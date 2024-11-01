248bac80f91da7d4a8e5603bd1d11075dee24963
Jim Albright <37345113+albrja@users.noreply.github.com>
2024-11-01 15:12:31 -0700

    Albrja/mic-5414/vtu-FuzzyChecker (#471)
    Albrja/mic-5414/vtu-FuzzyChecker
    
    Import FuzzyChecker from VTU
    - *Category*: Test
    - *JIRA issue*: [MIC-5414](https://jira.ihme.washington.edu/browse/MIC-5414)
    
    -import FuzzyChecker from VTU
    
    Testing
    All tests pass

--- tests/unit/test_column_noise.py
+++ tests/unit/test_column_noise.py
@@ -7,7 +7,7 @@ from collections.abc import Iterable
 import numpy as np
 import pandas as pd
 import pytest
-from layered_config_tree import LayeredConfigTree
+from vivarium_testing_utils import FuzzyChecker
 
 from pseudopeople.configuration import Keys, get_configuration
 from pseudopeople.configuration.noise_configuration import NoiseConfiguration
@@ -23,7 +23,6 @@ from pseudopeople.utilities import (
     load_qwerty_errors_data,
     to_string,
 )
-from tests.conftest import FuzzyChecker
 
 CHARACTERS_LIST = [
     "A",
