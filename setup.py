17570d50ccb803809280b50ac4603c812d49aedf
Hussain Jafari <hussainsjafari@gmail.com>
2024-11-22 11:37:15 -0800

    add memory_profiler

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ if __name__ == "__main__":
         "pytest",
         "pytest-mock",
         "vivarium_testing_utils",
+        "memory_profiler",
     ] + dask_requirements
 
     lint_requirements = [
