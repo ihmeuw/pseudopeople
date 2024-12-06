931d7230127612f0ce4f9cca7a0d279855347751
Hussain Jafari <hussainsjafari@gmail.com>
2024-12-06 14:35:07 -0800

    parametrizable data fixtures (#479)
    Category: feature
    JIRA issue: MIC-5519
    
    Create a dataset fixture which will load data of interest.
    Add options for parameters to pytest.
    Add tests that command line args are parsed correctly.
    
    Testing
    Tests pass, including new tests.

--- setup.py
+++ setup.py
@@ -73,6 +73,7 @@ if __name__ == "__main__":
         "pytest",
         "pytest-mock",
         "vivarium_testing_utils",
+        "memory_profiler",
     ] + dask_requirements
 
     lint_requirements = [
