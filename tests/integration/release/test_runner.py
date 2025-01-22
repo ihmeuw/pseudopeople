f03ae2e0f7a2cd01bffd07f5aadd0522d0adf837
Hussain Jafari <hussainsjafari@gmail.com>
2025-01-22 14:00:39 -0800

    define log path

--- tests/integration/release/test_runner.py
+++ tests/integration/release/test_runner.py
@@ -17,12 +17,14 @@ import pytest
         # (["--dataset", "wic", "--population", "USA", "--state", "RI", "--year", "2015"]),
     ],
 )
-def test_release_tests(pytest_args: list[str]) -> None:
+def test_release_tests(pytest_args: list[str], release_output_dir: Path) -> None:
     os.chdir(Path(__file__).parent)  # need this to access cli options from conftest.py
     base_cmd = ["pytest", "--release", "test_release.py"]
     cmd = base_cmd + pytest_args
-    result = subprocess.run(cmd, capture_output=True, text=True)
-    assert result.returncode == 0
+    # Open a file in write mode
+    with open(f"{release_output_dir}/pytest.new", 'w') as log_file:
+        # Run pytest and direct stdout to the log file
+        subprocess.run(cmd, stdout=log_file)
 
 
 @pytest.mark.parametrize("dataset", ["acs", "cps"])
