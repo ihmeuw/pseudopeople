53acc8972cb6ea8a388d54ba4358c2313682f753
Steve Bachmeier <sbachmei@uw.edu>
2025-02-04 13:18:18 -0800

    debugging print statements

--- update_readme.py
+++ update_readme.py
@@ -15,12 +15,14 @@ with open("README.rst", "r") as file:
     readme = file.read()
 # Update the list of supported python versions
 # NOTE: this regex assumes the version format is always major.minor
+print(versions_str)
+print(readme)
 readme = re.sub(
     r"Supported Python versions:\s*(?:\d+\.\d+\s*,\s*)+\d+\.\d+",
     r"Supported Python versions: " + versions_str,
     readme,
 )
-
+print(readme)
 # Write the updated README.md back to file
 with open("README.rst", "w") as file:
     file.write(readme)
