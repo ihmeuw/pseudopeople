01641700388e0b8407ee1935fe979e9e79ec8413
Steve Bachmeier <sbachmei@uw.edu>
2025-02-06 11:34:59 -0800

    handle python versions

--- update_readme.py
+++ update_readme.py
@@ -5,23 +5,21 @@ the project. It is intended to be run from the github "update README" workflow.
 import json
 import re
 
-from packaging.version import parse
-
 # Load supported python versions
 with open("python_versions.json", "r") as f:
     versions = json.load(f)
 versions_str = ", ".join(versions)
-versions = [parse(v) for v in versions]
-max_version = max(versions).base_version
 
-# Open README.md and replace the line containing the python versions
-
-# Replace python versions in readme
+# Open README.md and replace supported python versions line
 with open("README.rst", "r") as file:
     readme = file.read()
-readme = re.sub(r"python=\d+\.\d+", "python=" + max_version, readme)
+
+# Update the list of supported python versions
+# NOTE: this regex assumes the version format is always major.minor
 readme = re.sub(
-    r"Supported Python versions: .*", r"Supported Python versions: " + versions_str, readme
+    r"Supported Python versions:\s*(?:\d+\.\d+(?:\s*,\s*\d+\.\d+)*)",
+    r"Supported Python versions: " + versions_str,
+    readme,
 )
 
 # Write the updated README.md back to file
