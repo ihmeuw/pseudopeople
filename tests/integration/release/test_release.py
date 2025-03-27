029944c388e97f378ee4cc253083dc862cace5ef
Hussain Jafari <hjafari@uw.edu>
2025-03-27 13:59:07 -0700

    lint

--- tests/integration/release/test_release.py
+++ tests/integration/release/test_release.py
@@ -132,7 +132,7 @@ def test_do_not_respond(
         observed_numerator=len(original_data) - len(noised_data),
         observed_denominator=len(original_data),
         # 3% uncertainty on either side
-        target_proportion=(expected_noise *.97, expected_noise*1.03),
+        target_proportion=(expected_noise * 0.97, expected_noise * 1.03),
         name_additional=f"noised_data",
     )
     assert set(noised_data.columns) == set(original_data.columns)
