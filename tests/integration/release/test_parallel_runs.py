11e5f43aab441fb4545bd6faa2c24fd450ad81af
Hussain Jafari <hjafari@uw.edu>
2025-06-26 10:07:10 -0700

    skip instead of commenting out test file

--- tests/integration/release/test_parallel_runs.py
+++ tests/integration/release/test_parallel_runs.py
@@ -1,48 +1,52 @@
-# import time
-# from collections.abc import Callable
-# from pathlib import Path
-# from typing import Literal
+import time
+from collections.abc import Callable
+from pathlib import Path
+from typing import Literal
 
-# import pandas as pd
-# from pytest_check import check
-
-
-# def test_all_passing() -> None:
-#     """Test which passes no matter what parameters are called."""
-#     pass
+import pandas as pd
+import pytest
+from pytest_check import check
 
 
-# def test_some_failing(
-#     dataset_params: tuple[
-#         str,
-#         Callable[..., pd.DataFrame],
-#         Path | str | None,
-#         int | None,
-#         str | None,
-#         Literal["pandas", "dask"],
-#         str,
-#     ],
-# ) -> None:
-#     """Test which fails for some but not all parameters."""
-#     dataset_name, _, _, _, _, _, _ = dataset_params
-#     if dataset_name == "census":
-#         with check:
-#             assert False
+@pytest.mark.skip(reason="useful for testing launcher, but not used as actual test")
+def test_all_passing() -> None:
+    """Test which passes no matter what parameters are called."""
+    pass
 
 
-# def test_cancellation(
-#     dataset_params: tuple[
-#         str,
-#         Callable[..., pd.DataFrame],
-#         Path | str | None,
-#         int | None,
-#         str | None,
-#         Literal["pandas", "dask"],
-#         str,
-#     ],
-# ) -> None:
-#     """Test which will get cancelled for tax_1040 when job is called by sbatch
-#     with a 1 minute srun."""
-#     dataset_name, _, _, _, _, _, _ = dataset_params
-#     if dataset_name == "tax_1040":
-#         time.sleep(60 * 5)  # 5 minutes
+@pytest.mark.skip(reason="useful for testing launcher, but not used as actual test")
+def test_some_failing(
+    dataset_params: tuple[
+        str,
+        Callable[..., pd.DataFrame],
+        Path | str | None,
+        int | None,
+        str | None,
+        Literal["pandas", "dask"],
+        str,
+    ],
+) -> None:
+    """Test which fails for some but not all parameters."""
+    dataset_name, _, _, _, _, _, _ = dataset_params
+    if dataset_name == "census":
+        with check:
+            assert False
+
+
+@pytest.mark.skip(reason="useful for testing launcher, but not used as actual test")
+def test_cancellation(
+    dataset_params: tuple[
+        str,
+        Callable[..., pd.DataFrame],
+        Path | str | None,
+        int | None,
+        str | None,
+        Literal["pandas", "dask"],
+        str,
+    ],
+) -> None:
+    """Test which will get cancelled for tax_1040 when job is called by sbatch
+    with a 1 minute srun."""
+    dataset_name, _, _, _, _, _, _ = dataset_params
+    if dataset_name == "tax_1040":
+        time.sleep(60 * 5)  # 5 minutes
